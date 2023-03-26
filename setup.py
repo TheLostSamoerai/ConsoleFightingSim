4fe86ec173bd994b3c7d4280980d5ca2613b862b
simonbuyck <simonbuyck@icloud.com>
2023-03-26 10:09:48 +0200

    hello

--- setup.py
+++ setup.py
@@ -125,6 +125,7 @@ def loading_players_dialogue(filename):
     print("WELCOME TO THE LOADER!")
     print("")
     players = load_player(filename)
+    # printing all players available to load
     for i, player in enumerate(players):
         print(f"{i+1}. {player.name}")
     player_choice = int(input("Which player would you like to load? Enter the number of the player: "))
@@ -136,8 +137,10 @@ def saving_players_dialogue(p1, p2, filename):
     user_input = input("Would you like to save p1 or p2 (typ p1 or p2 or both): ")
     if "p1" in user_input:
         save_player(p1, filename)
+        print("player 1 saved succesfully")
     elif "p2" in user_input:
         save_player(p2, filename)
+        print("saved player 2 succesfully")
     else:
         save_player(p1, filename)
         save_player(p2, filename)
